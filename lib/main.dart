6d7daf8394900cc17d39bbee89649705e0e79bf5
Jonathan Jr. Peguit <jonathanjrhayo@gmail.com>
2025-05-21 09:39:55 +0800

    Error Fixed
    Fixed the error from gradle and adding the splash screen to the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:shoe_buy/utils/theme/theme.dart';
 import 'package:shoe_buy/app.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-
-  // Todo: Add widget binding
+void main() async {
+  // Add widget binding - required for splash screen
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  
+  // Preserve splash screen until app is loaded
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  
   // Todo: Initialize local storage
-  // todo: await native splash
   // todo: initialkize firebase
   // todo: initialiuze authentication
+
+  // Remove splash screen when app is ready
+  FlutterNativeSplash.remove();
 
   runApp(const App());
 }
