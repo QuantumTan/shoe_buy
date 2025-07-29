23d05ab7c8d81c63477ca5c0ca3082b43465f9de
Jonathan Peguit Jr. <jonathanjrhayo@gmail.com>
2025-07-29 09:49:05 +0800

    add dark mode support in navigation menu and clean up color constants

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shoe_buy/utils/constants/scolors.dart';
+import 'package:shoe_buy/utils/helpers/helper_functions.dart';
 
 class NavMenu extends StatelessWidget {
   const NavMenu({super.key});
@@ -9,6 +11,7 @@ class NavMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
+    final darkMode = SHelperFunctions.isDarkMode(context);
 
     //obs: Used to create an observable variable that can be monitored for changes.
     //Obx: A widget that listens to observable variables and rebuilds its child whenever the observed variable changes.
@@ -21,6 +24,12 @@ class NavMenu extends StatelessWidget {
           selectedIndex: controller.selectedIndex.value,
           onDestinationSelected:
               (index) => controller.selectedIndex.value = index,
+
+          backgroundColor: darkMode ? SColor.black : SColor.white,
+          indicatorColor:
+              darkMode
+                  ? SColor.white.withValues(alpha: 0.1)
+                  : SColor.black.withValues(alpha: 0.1),
 
           destinations: [
             const NavigationDestination(
--- lib/utils/constants/scolors.dart
+++ lib/utils/constants/scolors.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class SColor{
+class SColor {
   SColor._();
 
   static const Color primaryColor = Color(0xFF4b68ff);
   static const Color secondaryColor = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
-  static const Color dark =  Color.fromARGB(255, 168, 168, 168);
+  static const Color dark = Color.fromARGB(255, 168, 168, 168);
   static const Color light = Color.fromARGB(255, 65, 64, 64);
 
-  
+  static const Color white = Color.fromARGB(255, 245, 245, 245);
+  static const Color black = Color.fromARGB(255, 18, 18, 18);
 }
