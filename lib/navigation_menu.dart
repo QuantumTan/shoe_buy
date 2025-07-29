6db31838800879141da92c2225bd5ad0878dfa4e
Jonathan Peguit Jr. <jonathanjrhayo@gmail.com>
2025-07-29 08:33:59 +0800

    implement reactive navigation bar with observable controller

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -7,32 +8,53 @@ class NavMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        height: 80,
-        elevation: 0,
-        selectedIndex: 0,
-        //TODO
-        onDestinationSelected: (index),
+    final controller = Get.put(NavigationController());
 
-        destinations: [
-          const NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-          const NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
-          const NavigationDestination(
-            icon: Icon(Iconsax.heart1),
-            label: 'Wishlists',
-          ),
-          const NavigationDestination(
-            icon: Icon(Iconsax.user),
-            label: 'Profile',
-          ),
-        ],
+    //obs: Used to create an observable variable that can be monitored for changes.
+    //Obx: A widget that listens to observable variables and rebuilds its child whenever the observed variable changes.
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected:
+              (index) => controller.selectedIndex.value = index,
+
+          destinations: [
+            const NavigationDestination(
+              icon: Icon(Iconsax.home),
+              label: 'Home',
+            ),
+            const NavigationDestination(
+              icon: Icon(Iconsax.shop),
+              label: 'Store',
+            ),
+            const NavigationDestination(
+              icon: Icon(Iconsax.heart),
+              label: 'Wishlists',
+            ),
+            const NavigationDestination(
+              icon: Icon(Iconsax.user),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
-      body: Container(),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
 }
 
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
+
+  //sample screens to view the code's logic
+  final screens = [
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+    Container(color: Colors.purple),
+    Container(color: Colors.orange),
+  ];
 }
